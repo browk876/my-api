d22cbed755f0f3fdef9169cc51fe6dad1c84a24b
browk876 <browk876@newschool.edu>
2023-12-04 13:34:40 -0500

    c2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const axios = require('axios');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
 
+
+
+import express from 'express';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 5500;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/random-animal', async (req, res) => {
     try {
@@ -14,7 +28,10 @@ app.get('/random-animal', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
+// Set the application to listen on a port
+app.listen(port, () => {
+    console.log(`Wskb app listening on port ${port}`);
+});
 
 async function getRandomAnimalImage() {
     const loremPicsumUrl = 'https://picsum.photos/200/300/?random';
