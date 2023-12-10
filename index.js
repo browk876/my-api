3b28649d4f88059b919774144baacfb2c01636c3
browk876 <browk876@newschool.edu>
2023-12-10 18:42:32 -0500

    cors update

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 5500;
 
-// Serve static files including client.js
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) => {
